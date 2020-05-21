0ebcb43fe6647c09be640ff72fe681fbb374f4a1
Edwin Chow <echow6@gmail.com>
2020-05-20 20:46:27 -0500

    add build

---
+++ build/precache-manifest.c2288ec21db56e1d1692c74ba8bbcf70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548bcb7c3a5f6bb62742d6599bdc60ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f76768f55ae4fa1f74c7",
+    "url": "/static/css/main.eea5ab8a.chunk.css"
+  },
+  {
+    "revision": "68bc6edc749d7a496b4e",
+    "url": "/static/js/2.d695faf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d695faf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76768f55ae4fa1f74c7",
+    "url": "/static/js/main.7987ed44.chunk.js"
+  },
+  {
+    "revision": "74eb628b2292604e1b0d",
+    "url": "/static/js/runtime-main.ecf5e756.js"
+  }
+]);
